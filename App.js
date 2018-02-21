b9783d3490562911c99c11d3e3ad5faa6ceb50ed
Laptop FT <felipe.fuertes@gmail.com>
2018-02-21 18:42:40 -0400

    feat: App Navigation

--- App.js
+++ App.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { StackNavigator } from 'react-navigation';
+
+import reducers from './reducers';
+import DeckList from './components/DeckList';
+import Deck from './components/Deck';
+import Quiz from './components/Quiz';
+
+function AppStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
+
+const headerStyles = {
+  headerTintColor: '#ffffff',
+  headerStyle: {
+    backgroundColor: '#000000',
+  }
+}
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: DeckList,
+    navigationOptions: {
+      title: 'MobileFlashCards',
+      ...headerStyles
+    },
+  },
+  Deck: {
+    screen: Deck,
+    navigationOptions: headerStyles
+  },
+  Quiz: {
+    screen: Quiz,
+    navigationOptions: headerStyles
+  },
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={{flex: 1}}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
