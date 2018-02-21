ca943601497157c6d8861452513fdf50893fa033
Laptop FT <felipe.fuertes@gmail.com>
2018-02-21 18:44:40 -0400

    Helpers

---
+++ utils/api.js
@@ -0,0 +1,15 @@
+export const getDecks = () => {
+
+}
+
+export const getDeck = () => {
+    
+}
+
+export const saveDeckTitle = () => {
+    
+}
+
+export const addCardToDeck = () => {
+    
+}
---
+++ utils/colors.js
@@ -0,0 +1,4 @@
+export const black = '#000000';
+export const white = '#ffffff';
+export const green = '#00aa00';
+export const red = '#aa0000';
