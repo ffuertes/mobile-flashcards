571ef7326920377cf9059debc384f9a15274c404
Laptop FT <felipe.fuertes@gmail.com>
2018-02-22 21:11:23 -0400

    refactor: DeckList FlatList implementation

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Entypo } from '@expo/vector-icons'
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 import DeckItem from './DeckItem';
 
+import { getDecks } from '../utils/api';
+import { receiveDecks } from '../actions';
+
 class DeckList extends Component {
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        getDecks()
+            .then( decks => {
+                dispatch( receiveDecks(decks) )
+            });
+    }
+    
+    _keyExtractor = (item, index) => index;
+
     render() {
-        const { decks } = this.props;
+        const { decks, navigation } = this.props;
         return (
-            <View style={styles.container}>
-                { Object.keys(decks).map( name => <DeckItem key={name} deck={decks[name]} /> ) }
+            <View style={{flex: 1}}>
+
+                <FlatList
+                    style={ styles.container }
+                    data={Object.keys(decks)}
+                    keyExtractor={this._keyExtractor}
+                    renderItem={ ({item, index}) => <DeckItem deck={decks[item]} /> }
+                />
+                
+                <TouchableOpacity onPress={ () => navigation.navigate( 'AddDeck' ) } style={styles.addButton} >
+                    <View>
+                        <Entypo name='plus' size={30} color="#fff" />
+                    </View>
+                </TouchableOpacity>
+
             </View>
         )
     }
@@ -22,6 +50,26 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         padding: 20,
     },
+    addButton: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1a1a1a',
+        width: 70,
+        height: 70,
+        borderRadius: 40,
+    },
+    shadow: {
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1.0,
+    }
 });
 
 function mapStateToProps(decks) {
