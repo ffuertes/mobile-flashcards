78e4bb739826adba27e53f49838e87eadaaedac1
Laptop FT <felipe.fuertes@gmail.com>
2018-02-22 21:10:29 -0400

    feat: Local Storage

--- utils/api.js
+++ utils/api.js
@@ -1,15 +1,60 @@
+import { AsyncStorage } from 'react-native';
+
+const DECKS_STORAGE_KEY = 'FlashCards:decks';
+
 export const getDecks = () => {
-
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then( results => { 
+            return results === null 
+                ? setDummyData()
+                : JSON.parse( results ) 
+        });
 }
 
-export const getDeck = () => {
-    
+export const saveDeckTitle = name => {
+    return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [name]: {
+            title: name,
+            questions: [],
+        }
+    }))
 }
 
-export const saveDeckTitle = () => {
-    
+export const addCardToDeck = (deck, question, answer) => {
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then((results) => {
+            const decks = JSON.parse(results)
+            decks[deck].questions.push({
+                question,
+                answer,
+            });
+            AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(decks))
+        });
 }
 
-export const addCardToDeck = () => {
-    
+function setDummyData() {
+    return {
+        React: {
+          title: 'React',
+          questions: [
+            {
+              question: 'What is React?',
+              answer: 'A library for managing user interfaces'
+            },
+            {
+              question: 'Where do you make Ajax requests in React?',
+              answer: 'The componentDidMount lifecycle event'
+            }
+          ]
+        },
+        JavaScript: {
+          title: 'JavaScript',
+          questions: [
+            {
+              question: 'What is a closure?',
+              answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+          ]
+        }
+    }
 }
